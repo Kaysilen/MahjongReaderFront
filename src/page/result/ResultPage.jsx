d8f2f8f7e84d40838ee2c4353bdf86edbfc2c766
오성돈 <sungdon_oj@tmax.co.kr>
2021-03-25 17:01:24 +0900

    [feat] 기본 페이지 작성.

---
+++ src/page/result/ResultPage.jsx
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles({
+
+});
+
+
+const ResultPage = (props) => {
+
+
+    return (
+        <div>
+            Home Page!
+        </div>
+    );
+}
+
+
+export default ResultPage;
